12a8c42acb79da1709396c1a574f9cb66ecc7499
JIAHSIN\Thuan.Truong <tazpaul6631@gmail.com>
2026-06-20 14:12:04 +0700

    commit changes

--- src/api/baseURLApi.ts
+++ src/api/baseURLApi.ts
@@ -1,4 +1,4 @@
-// const urlServer = 'https://api_patrol.jiahsin.com.vn';
+const urlServer = 'http://192.168.0.164:7198';
 const urlLocal = 'http://10.0.111.127:7198';
 const urlLocalWifi = 'http://10.0.149.107:7198';
 
